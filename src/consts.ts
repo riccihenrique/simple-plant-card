0e7b83ea19d0ff8ac105b9d69d5d12726e7a06a1
Henrique Ricci <ricci.henrique@outlook.com>
2026-04-26 17:05:25 -0300

    bump card version

--- src/consts.ts
+++ src/consts.ts
@@ -4,6 +4,6 @@ export const CARD_TYPE="simple-plant-card";
 export const CARD_NAME="Simple Plant Card";
 export const CARD_DESCRIPTION="Custom card for simple-plant integration";
 
-export const CARD_VERSION="v1.2.3";
+export const CARD_VERSION="v1.3.0";
 export const CARD_AUTHOR="ndesgranges";
 
